a4e42d9cab80e1ca0077aad2d0db3361ab3e3bea
ahsouleymane <souleymaneah98@gmail.com>
2025-07-01 18:18:25 +0100

    first commit

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,13 +1,32 @@
 package com.ecom.controller;
 
 import org.springframework.stereotype.Controller;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+=======
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
+
+>>>>>>> 611a288 (frontend design and package creating)
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
+<<<<<<< HEAD
+=======
+	private CategoryService categoryService;
+	
+>>>>>>> 611a288 (frontend design and package creating)
 	@GetMapping("/")
 	public String index() {
 		return "admin/index";
@@ -22,5 +41,31 @@ public class AdminController {
 	public String category() {
 		return "admin/category";
 	}
+<<<<<<< HEAD
+=======
+	
+	@PostMapping("/saveCategory")
+	public String saveCategory(@ModelAttribute Category category, HttpSession session) {
+		
+		Boolean existCategory = categoryService.existCategory(category.getName());
+		
+		if(existCategory) {
+			session.setAttribute("errorMsg", "Category Name already exists");
+		} else {
+			
+			Category saveCategory = categoryService.saveCategory(category);
+			
+			if(ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Not saved ! internal server error");
+			} else {
+				session.setAttribute("succesMsg", "Saved successfully");
+			}
+
+		}
+		
+		return "redirect:/category";
+		
+	}
+>>>>>>> 611a288 (frontend design and package creating)
 
 }
