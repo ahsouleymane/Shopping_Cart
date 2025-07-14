ac6334d3172c008bf0704aec6b363700c52ef4dd
ahsouleymane <souleymaneah98@gmail.com>
2025-07-14 12:48:19 +0100

    view products fonctions and views implementation

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecom.model.Category;
 import com.ecom.model.Product;
 import com.ecom.service.CategoryService;
 import com.ecom.service.ProductService;
+import com.ecom.service.UserService;
 
 @Controller
 public class HomeController {
@@ -20,6 +23,9 @@ public class HomeController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/")
 	public String index() {
@@ -37,16 +43,20 @@ public class HomeController {
 	}
 	
 	@GetMapping("/products")
-	public String products(Model m) {
+	public String products(Model m, @RequestParam(value = "category", defaultValue = "") String category) {
+		System.out.println("category="+category);
 		List<Category> categories = categoryService.getAllActiveCategory();
-		List<Product> products = productService.getAllActiveProducts();
+		List<Product> products = productService.getAllActiveProducts(category);
 		m.addAttribute("categories", categories);
 		m.addAttribute("products", products);
+		m.addAttribute("paramValue", category);
 		return "product";
 	}
 	
-	@GetMapping("/product")
-	public String product() {
+	@GetMapping("/product/{id}")
+	public String product(@PathVariable int id, Model m) {
+		Product productById = productService.getProductById(id);
+		m.addAttribute("product", productById);
 		return "view_product";
 	}
 
